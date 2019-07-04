9755a283890e9913a4b0ccfebaf883966cff390a
ecdunan <dunan.emmanuel@gmail.com>
2019-07-04 08:23:29 +0800

    Added keypress event listener

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import './ImageLinkForm.css';
 
-function ImageLinkForm({ onInputChange, onDetect }) {
+function ImageLinkForm({ onInputChange, onDetect, onEnter }) {
     return (
         <div>
             <p className='f3'>
                 {'This Magic Brain will detect faces in your pictures. Git it a try'}
             </p>
             <div className='center'>
-                <div className='pa4 br3 shadow-5 center form'>
+                <div className='pa4 br5 shadow-5 center form '>
                     <input className='f4 pa2 w-70 center' type='text'
-                            onChange={ onInputChange }/>
+                            onChange={ onInputChange }
+                            onKeyPress= { onEnter }/>
                     <button className='w-30 f4 link ph3 pv2 dib white bg-light-purple grow pointer'
                             onClick={ onDetect }>
                             Detect!
