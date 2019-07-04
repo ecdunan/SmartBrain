47f3ed981872282d974a673a301d56c9680ebbf3
ecdunan <dunan.emmanuel@gmail.com>
2019-07-04 08:26:39 +0800

    Revert "Added keypress event listener"
    This reverts commit 9755a283890e9913a4b0ccfebaf883966cff390a.

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import './ImageLinkForm.css';
 
-function ImageLinkForm({ onInputChange, onDetect, onEnter }) {
+function ImageLinkForm({ onInputChange, onDetect }) {
     return (
         <div>
             <p className='f3'>
                 {'This Magic Brain will detect faces in your pictures. Git it a try'}
             </p>
             <div className='center'>
-                <div className='pa4 br5 shadow-5 center form '>
+                <div className='pa4 br3 shadow-5 center form'>
                     <input className='f4 pa2 w-70 center' type='text'
-                            onChange={ onInputChange }
-                            onKeyPress= { onEnter }/>
+                            onChange={ onInputChange }/>
                     <button className='w-30 f4 link ph3 pv2 dib white bg-light-purple grow pointer'
                             onClick={ onDetect }>
                             Detect!
