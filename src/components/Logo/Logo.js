fd238103418bcb1850154ab448ffea94bfc2ef4c
ecdunan <dunan.emmanuel@gmail.com>
2019-07-06 17:13:29 +0800

    fixed spaceing

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -6,7 +6,9 @@ function Logo() {
     return (
             <div className='ma4 mt0'>
                 <Tilt className="Tilt br3 shadow-5" options={{ max : 55 }} style={{ height: 100, width: 100 }} >
-                    <div className="Tilt-inner pa2"> <img src={brain} alt='logo'/></div>
+                    <div className="Tilt-inner pa2">
+                        <img src={brain} alt='logo'/>
+                    </div>
                 </Tilt>
             </div>
         );
